e507caaaca0b3dbaf64db805a1062eaff98955d5
yesid.herrera <yesid.herrera@wabtec.com>
2024-12-06 19:55:01 -0500

    project scaffolding

--- apps/siscontsi/src/app/app.config.ts
+++ apps/siscontsi/src/app/app.config.ts
@@ -1,10 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
+import { provideIonicAngular } from '@ionic/angular/standalone';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
+    provideRouter(appRoutes), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), provideIonicAngular({}),
   ],
 };
